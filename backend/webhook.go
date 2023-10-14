c2a23cafaa1018b27364f4d8a6ba3c5a4c5bfdb1
Sasaki-Motoaki <voyegesansretour1@gmail.com>
2023-10-14 16:24:03 +0900

    webhookのイベントごとのハンドリングをするための構造を作成し、細くコメント追加

--- backend/webhook.go
+++ backend/webhook.go
@@ -32,9 +32,37 @@ func handleWebhook(w http.ResponseWriter, req *http.Request) {
 		w.WriteHeader(http.StatusBadRequest) // Return a 400 error on a bad signature
 		return
 	}
-
-	// Unmarshal the event data into an appropriate struct depending on its Type
-	fmt.Fprintf(os.Stderr, "Unhandled event type: %s\n", event.Type)
+	// TODO: イベントの種類によって処理を変える
+	// checkoutで購入完了した時のイベント
+	// stripe.EventTypeCheckoutSessionCompleted
+	// https://stripe.com/docs/payments/checkout/how-checkout-works#complete
+	// checkoutでセッションの期限が切れた時のイベント。在庫確保する系なら、セッション作成後に在庫を確保し、これを機に在庫を戻す
+	// stripe.EventTypeCheckoutSessionExpired
+	switch event.Type {
+	// case stripe.EventTypePaymentIntentSucceeded:
+	// 	var paymentIntent stripe.PaymentIntent
+	// 	err := json.Unmarshal(event.Data.Raw, &paymentIntent)
+	// 	if err != nil {
+	// 		fmt.Fprintf(os.Stderr, "Error parsing webhook JSON: %v\n", err)
+	// 		w.WriteHeader(http.StatusBadRequest)
+	// 		return
+	// 	}
+	// 	// Then define and call a func to handle the successful payment intent.
+	// 	// handlePaymentIntentSucceeded(paymentIntent)
+	// case stripe.EventTypePaymentMethodAttached:
+	// 	var paymentMethod stripe.PaymentMethod
+	// 	err := json.Unmarshal(event.Data.Raw, &paymentMethod)
+	// 	if err != nil {
+	// 		fmt.Fprintf(os.Stderr, "Error parsing webhook JSON: %v\n", err)
+	// 		w.WriteHeader(http.StatusBadRequest)
+	// 		return
+	// 	}
+	// 	// Then define and call a func to handle the successful attachment of a PaymentMethod.
+	// 	// handlePaymentMethodAttached(paymentMethod)
+	// // ... handle other event types
+	default:
+		fmt.Fprintf(os.Stderr, "Unhandled event type: %s\n", event.Type)
+	}
 
 	w.WriteHeader(http.StatusOK)
 }
